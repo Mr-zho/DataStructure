9283f971068a3fdf3e9a288e0cb9af405c11cfd0
wenliang <wenliang@163.com>
2023-12-14 22:58:53 +0800

    [修改]构建普通数据类型插入到动态数组的错误场景

--- DynamicArray/main.c
+++ DynamicArray/main.c
@@ -4,7 +4,7 @@
 #include <pthread.h>
 #include <string.h>
 
-#define DEFAULT_NUM    2
+#define DEFAULT_NUM    4
 #define BUFFER_SIZE    20
 
 typedef struct StuInfo
@@ -16,7 +16,7 @@ typedef struct StuInfo
 
 int main(int argc, const char *argv[])
 {
-#if 0
+#if 1
     DynamicArray array;
     /* 初始化 */
     dynamicArrayInit(&array);
@@ -24,28 +24,37 @@ int main(int argc, const char *argv[])
     dynamicArrayGetCapacity(&array, &arrayCapacity);
     printf("arrayCapacity:%d\n", arrayCapacity);
 
-
-    /* 插入 */
-    for (int idx = 1; idx <= DEFAULT_NUM << 1; idx++)
+#if 0
+    /* 这是错误场景,请思考为什么？*/
+    int idx = 1;
+    for (idx; idx <= DEFAULT_NUM; idx++)
     {
-        dynamicArrayInsert(&array, idx);
+        dynamicArrayInsert(&array, (void *)&idx);
     }
-    dynamicArrayInsert(&array, 666);
-
+#else
+    /* 插入 */
+    int nums[DEFAULT_NUM];
+    /* 清空脏数据 */
+    memset(nums, 0, sizeof(nums));
+    for (int idx = 0; idx < DEFAULT_NUM; idx++)
+    {
+        nums[idx] = idx + 100;
+    }
+    for (int idx = 0; idx < DEFAULT_NUM; idx++)
+    {
+        dynamicArrayInsert(&array, (void *)&nums[idx]);
+    }
+#endif
     int arraySize = 0;
     dynamicArrayGetSize(&array, &arraySize);
     printf("arraySize:%d\n", arraySize);
 
-    arrayCapacity = 0;
-    dynamicArrayGetCapacity(&array, &arrayCapacity);
-    printf("arrayCapacity:%d\n", arrayCapacity);
-
     /* 打印动态数组数据 */
-    int val = 0;
+    int *val = NULL;
     for (int idx = 0; idx < arraySize; idx++)
     {
-        dynamicArrayGetAppointPosVal(&array, idx, &val);
-        printf("val:%d\t", val);
+        dynamicArrayGetAppointPosVal(&array, idx, (void *)&val);
+        printf("val:%d\t", *(int*)val);
     }
     printf("\n");
 
@@ -61,6 +70,8 @@ int main(int argc, const char *argv[])
     }
     printf("\n");
 #endif
+
+#if 0
     for (int idx = 0; idx < DEFAULT_NUM + 5; idx++)
     {
         dynamicArrayDelAppointPos(&array, 0);
@@ -71,45 +82,38 @@ int main(int argc, const char *argv[])
     dynamicArrayGetCapacity(&array, &arrayCapacity);
     printf("arrayCapacity:%d\n", arrayCapacity);
 
-
     for (int idx = 0; idx < arraySize; idx++)
     {
-        dynamicArrayGetAppointPosVal(&array, idx, &val);
-        printf("val:%d\t", val);
+        dynamicArrayGetAppointPosVal(&array, idx, (void *)&val);
+        printf("val:%d\t", *val);
     }
     printf("\n");
-
+#endif
     
     dynamicArrayDestory(&array);
 #endif
+
+#if 0
     StuInfo stu1;
     StuInfo stu2;
     StuInfo stu3;
-    // memset(&stu1, 0, sizeof(stu1));
-    // memset(&stu2, 0, sizeof(stu2));
-    // memset(&stu3, 0, sizeof(stu3));
+    memset(&stu1, 0, sizeof(stu1));
+    memset(&stu2, 0, sizeof(stu2));
+    memset(&stu3, 0, sizeof(stu3));
 
     /* 模块化初始化 */
-#if 1
     {
         stu1.age = 10;
         stu1.sex = 'm';
         char *name = "yuchen";
         strncpy(stu1.name, name, sizeof(stu1.name) - 1);
     }
-#endif
-
-#if 1
-    /* 模块化初始化 */
     {
         stu2.age = 20;
         stu2.sex = 'f';
         char *name = "mr_shuo";
         strncpy(stu2.name, name, sizeof(stu2.name) - 1);
     }
-#endif
-
-#if 1
     /* 模块化初始化 */
     {
         stu3.age = 30;
@@ -119,7 +123,7 @@ int main(int argc, const char *argv[])
     }
 #endif
 
-#if 1
+#if 0
     /* 动态数组 */
     DynamicArray array;
     /* 初始化 */
@@ -140,16 +144,13 @@ int main(int argc, const char *argv[])
     int arraySize = 0;
     dynamicArrayGetSize(&array, &arraySize);
     printf("arraySize:%d\n", arraySize);
-#endif
 
-#if 1
     int pos = 0;
     StuInfo * info = NULL;
     for (int idx = 1; idx < (DEFAULT_NUM << 1); idx++)
     {
         dynamicArrayGetAppointPosVal(&array, idx - 1, (void*)&info);
-        printf("info.age = %d\n", info->age);
-        printf("info.age = %c\n", info->sex);
+        printf("info.age = %d\tinfo.sex:%c\n", info->age, info->sex);
     }
 #endif
     return 0;
